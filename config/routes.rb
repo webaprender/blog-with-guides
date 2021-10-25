a6890d741831beec68a8d90cd0065dfa120fda9a
webaprender <webaprendertv@gmail.com>
2021-10-25 03:39:39 -0500

    6.1 Showing a Single Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'articles/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'articles#index'
     get '/articles/index'
+    
+    #6.1 Showing a Single Article
+    get 'articles/:id', to: 'articles#show'
 end
