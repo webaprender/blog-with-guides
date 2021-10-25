990b578a155482838df9ae3f18b2e01baffce4f7
webaprender <webaprendertv@gmail.com>
2021-10-25 03:48:43 -0500

    anexando resources para route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  root 'articles#index'
-    get '/articles/index'    
-    
-    #6.1 Showing a Single Article
-    get 'articles/:id', to: 'articles#show'
+  root "articles#index"
+
+  resources :articles
 end
